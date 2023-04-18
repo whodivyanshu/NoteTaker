06c062c23ae3a9a63fc49a517d9ba83e43c76041
DivyanshuLohar <divyanshulohar2002@gmail.com>
2023-04-18 15:00:14 +0000

    backend commit

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 const Main = () => {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    const response = await fetch("/create",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password
+      }),
+
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+
   return (
     <div className="main">
         <div className="mleft">
             <div className="mlogin">
 
                 <h1>Welcome to NoteTaker</h1>
-              <form action="/" method="post">
-                <input type="text" placeholder='username' />
+              <form action="/" method="post" onSubmit={handleSubmit}>
+                <input type="text" placeholder='username' value={username} onChange={(e)=> setUsername(e.target.value)} />
                 <br />
-                <input type="password" placeholder='password' />
+                <input type="password" placeholder='password' value={password} onChange={(e)=> setPassword(e.target.value)} />
                 <p>Forgot Password</p>
 
                 <br />
-                <button>Sign In</button>
+                <button type='submit'>Sign In</button>
               </form>
             </div>
         </div>
--- server/index.js
+++ server/index.js
@@ -2,9 +2,42 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
-
+const uuid = require("uuid");
 const app = express();
+
+app.use(cors());
+
+
+const MONGODB_URL = "mongodb+srv://divyanshu21:8385@notesapp.qiog12x.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("mongodb connected"))
+  .catch((err) => console.log(err));
+
+const userSchema = new mongoose.Schema({
+    _id: String,
+    username: String,
+    password: String
+
+})
+
+const User = mongoose.model("User", userSchema);
+
+app.post("/login", (req,res)=>{
+    const newUser = new User({
+        _id: uuid.v4(),
+        name: req.body.username,
+        password: req.body.password
+    })
+    newUser.save();
+    res.send({status: "ok"});
+});
+
+
 
 app.get("/", (req,res)=>{
     res.send("hello world");
